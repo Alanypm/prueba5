30e7453f03b7fd29e9d1a774cd67bcb7ffb059a9
Alanypm <81442841+Alanypm@users.noreply.github.com>
2021-04-07 08:20:56 -0500

    Primer commit

---
+++ js/init.js
@@ -0,0 +1,10 @@
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyDjNGLMNZCDb6B6-DOJDBPKRhZ3Hd1qssw",
+  authDomain: "aypmprueba.firebaseapp.com",
+  databaseURL: "https://aypmprueba-default-rtdb.firebaseio.com",
+  projectId: "aypmprueba",
+  storageBucket: "aypmprueba.appspot.com",
+  messagingSenderId: "1078209308586",
+  appId: "1:1078209308586:web:d70c5d691d02ce63fe2dd3"
+});
